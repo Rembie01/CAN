2f0b53b2091c49f267114034a348203d56aed14b
Rembie01 <rembert.muylle@gmail.com>
2024-05-13 23:10:09 +0200

    removed chinese

--- models/infer_model.py
+++ models/infer_model.py
@@ -28,7 +28,6 @@ class Inference(nn.Module):
         self.device = params['device']
         self.decoder = decoder_dict[params['decoder']['net']](params=self.params)
 
-        """经过cnn后 长宽与原始尺寸比缩小的比例"""
         self.ratio = params['densenet']['ratio']
 
         with open(params['word_path']) as f:
